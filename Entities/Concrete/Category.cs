3e4a48b272277a69e80c2a24b8ab301bcae87f80
Beytullah Öztürk <ozbeytullah2@gmail.com>
2021-02-07 23:14:56 +0300

    07.02.2021 Ders Sonu

--- Entities/Concrete/Category.cs
+++ Entities/Concrete/Category.cs
@@ -1,12 +1,12 @@
-﻿
-using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Entities.Concrete
 {
-    // çıplak klas kalmasın "class Category" olmasın, "class Category:IEntity" olsun
+    // çıplak class kalmasın
+    // "class Category" olmasın, "class Category:IEntity" olsun
     public class Category:IEntity
     {
         public int CategoryId { get; set; }
